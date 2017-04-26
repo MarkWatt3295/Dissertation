dad1546a9f374dd7ffb4a77b42dd20ebb32c1023
MarkWatt3295 <maw79@aber.ac.uk>
2017-04-26 15:45:26 +0100

    Network Packet Change

--- MDKExample/src/main/java/com/maw79/mods/network/AdminCommandPacketHandler.java
+++ MDKExample/src/main/java/com/maw79/mods/network/AdminCommandPacketHandler.java
@@ -1,6 +1,9 @@
 package com.maw79.mods.network;
 
 import com.maw79.mods.init.ModItems;
+import com.maw79.mods.init.ModTools;
+
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentString;
@@ -23,11 +26,15 @@ World world = DimensionManager.getWorld(message.dimension);
  else if(!world.isRemote) {
  if(ctx.getServerHandler().playerEntity.getEntityId() == message.entityID){
  EntityPlayerMP player = ctx.getServerHandler().playerEntity;
- 
+ double x = player.posX;
+ double y = player.posY;
+ double z = player.posZ;
  	player.connection.playerEntity.inventory.clear();
-	player.connection.playerEntity.inventory.addItemStackToInventory(new ItemStack(ModItems.GAMETYPE, 1));
-	player.connection.playerEntity.inventory.addItemStackToInventory(new ItemStack(ModItems.SCOREDEBUG, 1));
-	 player.sendMessage(new TextComponentString("Admin Mode Enabled"));
+ 	 EntityItem item1 = new EntityItem(world, x, y, z, new ItemStack(ModTools.glassSpade));
+	 world.spawnEntity(item1);
+	//player.connection.playerEntity.inventory.addItemStackToInventory(new ItemStack(ModItems.GAMETYPE, 1));
+	//player.connection.playerEntity.inventory.addItemStackToInventory(new ItemStack(ModItems.SCOREDEBUG, 1));
+	player.sendMessage(new TextComponentString("§c"+"Admin Mode Enabled"));
 
 	
  }
